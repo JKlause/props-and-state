44729dfa4bce9556a8614c55114a6629871dfd41
Joe Klause <joeklause@gmail.com>
2019-10-30 14:04:29 -0700

    refactored, setting up random generator

--- src/containers/RandomColor.js
+++ src/containers/RandomColor.js
@@ -1,1 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ColorDisplay from '../components/colors/ColorDisplay';
+import ColorPicker from '../components/colors/ColorPicker';
+
+export default class RandomColor extends Component {
+  state = {
+    backgroundColor: 'black'
+  };
+
+  selectColor = backgroundColor => {
+    this.setState({ backgroundColor });
+  }
+  componentDidMount() {
+    console.log('I am mounted');
+  }
+
+  componentDidUpdate() {
+    console.log('I am updated');
+  }
+
+  componentWillUnmount() {
+    console.log('i am unmounted');
+  }
+  render() {
+    const { backgroundColor } = this.state;
+    const colors = ['red', 'green', 'black', 'yellow'];
+
+    return (
+      <>
+        <ColorDisplay backgroundColor={backgroundColor} />
+        <ColorPicker
+          selectColor={this.selectColor}
+          colors={colors} />
+      </>
+    );
+  }
+}
